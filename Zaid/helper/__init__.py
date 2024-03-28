b1416f02209bcda7a3bd7c3aa991a8a1fcc360a3
SACHIN SANATANI <159048376+SACHINxSANATANI@users.noreply.github.com>
2024-03-28 23:09:41 +0530

    Add files via upload

--- Zaid/helper/__init__.py
+++ Zaid/helper/__init__.py
@@ -1,1 +1,14 @@
+import os
+import sys
+from pyrogram import Client
 
+
+
+def restart():
+    os.execvp(sys.executable, [sys.executable, "-m", "Zaid"])
+
+async def join(client):
+    try:
+        await client.join_chat("i_m_fighter")
+    except BaseException:
+        pass
