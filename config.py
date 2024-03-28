8326adadc2cb0fe8e5a97075a208e6c05c66a0fd
SACHIN SANATANI <159048376+SACHINxSANATANI@users.noreply.github.com>
2024-03-28 23:26:29 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,33 @@
+import os
+from os import getenv
+from dotenv import load_dotenv
+
+if os.path.exists("local.env"):
+    load_dotenv("local.env")
+
+
+API_ID = int(getenv("API_ID", "6435225")) #optional
+API_HASH = getenv("API_HASH", "") #optional
+
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
+OWNER_ID = int(getenv("OWNER_ID"))
+MONGO_URL = getenv("MONGO_URL")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+ALIVE_PIC = getenv("ALIVE_PIC", 'https://telegra.ph/file/f5b0e7f2bd95153968e87.jpg')
+ALIVE_TEXT = getenv("ALIVE_TEXT")
+PM_LOGGER = getenv("PM_LOGGER")
+LOG_GROUP = getenv("LOG_GROUP")
+GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
+REPO_URL = getenv("REPO_URL", "https://github.com/itzshukla/STRANGER-USERBOT2.0")
+BRANCH = getenv("BRANCH", "master") #don't change
+ 
+STRING_SESSION1 = getenv("STRING_SESSION1", "")
+STRING_SESSION2 = getenv("STRING_SESSION2", "")
+STRING_SESSION3 = getenv("STRING_SESSION3", "")
+STRING_SESSION4 = getenv("STRING_SESSION4", "")
+STRING_SESSION5 = getenv("STRING_SESSION5", "")
+STRING_SESSION6 = getenv("STRING_SESSION6", "")
+STRING_SESSION7 = getenv("STRING_SESSION7", "")
+STRING_SESSION8 = getenv("STRING_SESSION8", "")
+STRING_SESSION9 = getenv("STRING_SESSION9", "")
+STRING_SESSION10 = getenv("STRING_SESSION10", "")
