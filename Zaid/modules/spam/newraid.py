2752dfbedcfcdc69325b28d84a27541f419ac304
SACHIN SANATANI <159048376+SACHINxSANATANI@users.noreply.github.com>
2024-05-29 22:09:31 +0530

    Create newraid.py

---
+++ Zaid/modules/spam/newraid.py
@@ -0,0 +1,43 @@
+import os
+import asyncio
+from pyrogram.types import Message
+from pyrogram import filters, Client
+from pyrogram.errors import FloodWait
+from ... import app, SUDO_USER
+from ... import *
+from cache.data import OneWord
+
+
+FC = 2
+
+
+@app.on_message(cdz(["randi"])  & (filters.me | filters.user(SUDO_USER)))
+async def alt_lol(xspam: Client, message: Message):    
+    chat_id = message.chat.id
+    RUSH = None
+    if message.reply_to_message:
+        RUSH = message.reply_to_message.id
+    try:
+        for word in OneWord:
+            await xspam.send_message(chat_id, word, reply_to_message_id=RUSH)
+            await asyncio.sleep(1)
+    except FloodWait:
+        print("Flood !!")
+        pass
+
+
+
+@app.on_message(cdz(["rrandi"])  & (filters.me | filters.user(SUDO_USER)))
+async def alt_mkc(xspam: Client, message: Message):    
+    chat_id = message.chat.id
+    RUSH = None
+    if message.reply_to_message:
+        RUSH = message.reply_to_message.id
+    try:
+        for word in OneWord:
+            await xspam.send_message(chat_id, word, reply_to_message_id=RUSH)
+            await asyncio.sleep(000.1)
+    except FloodWait:
+        print("Flood !!")
+        pass
+    
